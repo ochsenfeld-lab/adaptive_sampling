averages from mbar to utils, corrected units everywhere

adaptive_sampling/processing_tools/mbar.py
@@ -3,14 +3,15 @@ import sys
 import numpy as np
 from typing import List, Tuple
 
-from .utils import _error_vec, _join_frames
-
+from .utils import _join_frames
+from ..units import *
 
 def mbar(
     traj_list: List[np.ndarray],
     meta_f: np.ndarray,
     max_iter: int = 10000,
     conv: float = 1.0e-7,
+    conv_errvec: float = None,
     outfreq: int = 100,
     equil_temp: float = 300.0,
     dV_list: List[np.ndarray] = None,
@@ -21,7 +22,8 @@ def mbar(
         traj_list: List of biased trajectories
         meta_f: input from metafile
         max_iter: Maximum number of iterations
-        conv: Convergence criterion
+        conv: Convergence criterion, largest change in beta*Ai
+        conv_errvec: Convergence criterion based on the error vector, not used if None, largest absolute value in the error vec
         outfreq: Output frequency during self-consistent iteration
         equil_temp: Temperature of simulation
         dV_list: optional, list of GaMD boost potentials (has to match frames of traj_list)
@@ -29,9 +31,7 @@ def mbar(
     returns:
         W: array of statistical weigths of each frame
     """
-    H2kJmol = 2625.499639  # Hartree to kJ/mol
-    R = 8.314 / 1000.0  # kJ / K mol
-    RT = R * equil_temp / 1000.0
+    RT = R_in_SI * equil_temp / 1000.0
     beta = 1.0 / RT
 
     num_trajs = len(meta_f)
@@ -40,7 +40,7 @@ def mbar(
     all_frames, num_frames, frames_per_traj = _join_frames(traj_list)
     if dV_list is not None:
         all_dV, dV_num, dV_per_traj = _join_frames(dV_list)
-        all_dV *= H2kJmol  # kJ/mol
+        all_dV *= atomic_to_kJmol  # kJ/mol
         if (dV_num != num_frames) or (frames_per_traj != dV_per_traj).all():
             raise ValueError("GaMD frames have to match eABF frames!")
 
@@ -89,13 +89,19 @@ def mbar(
             print("Iter %4d:\tConv=%14.10f" % (count, np.max(delta_Ai[1:])))
             sys.stdout.flush()
 
+        max_err_vec = np.abs(_error_vec(frames_per_traj, beta_Ai, exp_U)).max()
+        if conv_errvec == None:
+            converged = delta_Ai[1:].max() < conv
+        else:
+            converged = (delta_Ai[1:].max() < conv) and (max_err_vec < conv_errvec)
+
         if count == max_iter:
             print(
                 "========================================================================"
             )
             print("Convergence not reached in {} iterations!".format(count))
             print(
-                "Max error vector:", (_error_vec(frames_per_traj, beta_Ai, exp_U).max())
+                "Max error vector:", max_err_vec
             )
             print(
                 "========================================================================"
@@ -103,13 +109,13 @@ def mbar(
             sys.stdout.flush()
             break
 
-        if delta_Ai[1:].max() < conv:
+        if converged:
             print(
                 "========================================================================"
             )
             print("Converged after {} iterations!".format(count))
             print(
-                "Max error vector:", (_error_vec(frames_per_traj, beta_Ai, exp_U).max())
+                "Max error vector:", max_err_vec
             )
             print(
                 "========================================================================"
@@ -145,8 +151,7 @@ def get_windows(
         index_list (np.ndarray): list of frame indices in original trajectory,
         meta_f (np.ndarray): window information for MBAR
     """
-    R = 8.314 / 1000.0  # kJ / K mol
-    RT = R * equil_temp / 1000.0
+    RT = R_in_SI * equil_temp / 1000.0
     k = RT / (sigma * sigma)
 
     dx = centers[1] - centers[0]
@@ -217,8 +222,7 @@ def pmf_from_weights(
     returns:
         Potential of mean force (PMF), probability density
     """
-    R = 8.314 / 1000.0  # kJ / K mol
-    RT = R * equil_temp / 1000.0
+    RT = R_in_SI * equil_temp / 1000.0
 
     dx = grid[1] - grid[0]
     dx2 = dx / 2.0
@@ -248,7 +252,7 @@ def deltaf_from_weights(
     returns:
         deltaF: free energy difference
     """
-    RT = R * equil_temp / 1000.0
+    RT = R_in_SI * equil_temp / 1000.0
     weights /= weights.sum()
 
     p_a = weights[np.where(cv < TS)].sum()
@@ -256,56 +260,14 @@ def deltaf_from_weights(
 
     return -RT * np.log(p_b / p_a)
 
-
-def ensemble_average(obs: np.ndarray, weights: np.ndarray) -> tuple:
-    """ensemble average of observable
-
-    args:
-        obs (np.ndarray): trajectory of observables
-        weights (np.ndarray): weigths of data frames
-
-    returns:
-        avg (float): ensemble average
-        sem (float): standard error of the mean of avg
-    """
-    weights /= weights.sum()
-    avg = np.average(obs, weights=weights)  # ensemble average
-    std = np.sqrt(
-        np.average(np.power(obs - avg, 2), weights=weights)
-    )  # standard deviation
-    sem = std / np.sqrt(float(len(obs)))  # standard error of the mean
-    return avg, sem
-
-
-def conditional_average(
-    grid: np.ndarray, xi_traj: np.ndarray, obs: np.ndarray, weights: np.ndarray
-) -> tuple:
-    """bin wise conditional average and standard error of the mean
-
-    args:
-        grid (np.ndarray): bins along reaction coordinate
-        xi_traj (np.ndarray): trajectory of reaction coordinate
-        obs (np.ndarray): trajectory of observables
-        weights (np.ndarray): weigths of data frames
-
-    returns:
-        obs_xi (np.ndarray): conditional average along reaction coordinate
-        sem_xi (np.ndarray): standard error of the mean of obs_xi
-    """
-    weights /= weights.sum()
-    dx = grid[1] - grid[0]
-    dx2 = dx / 2
-
-    obs_xi = np.zeros(len(grid))
-    sem_xi = np.zeros(len(grid))
-
-    for i, x in enumerate(grid):
-        dat_i = np.where(np.logical_and(xi_traj >= x - dx2, xi_traj < x + dx2))
-        nsamples = len(obs[dat_i])
-        if nsamples > 0:
-            p_i = weights[dat_i] / weights[dat_i].sum()
-            obs_xi[i] = np.sum(p_i * obs[dat_i])
-            sem_xi[i] = np.sqrt(
-                np.sum(p_i * np.square(obs[dat_i] - obs_xi[i]))
-            ) / np.sqrt(nsamples)
-    return obs_xi, sem_xi
+def _error_vec(
+    n_frames: np.ndarray, beta_Ai: np.ndarray, exp_U: np.ndarray
+) -> np.ndarray:
+    """error vector for MBAR"""
+    denominator = np.multiply(n_frames * np.exp(beta_Ai), exp_U.T)
+    denominator = 1.0 / np.sum(denominator, axis=1)
+    # sum over all frames
+    error_v = n_frames - n_frames * np.exp(beta_Ai) * (
+        np.multiply(exp_U, denominator).sum(axis=1)
+    )
+    return error_v

adaptive_sampling/processing_tools/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import Union, List, Tuple
+from ..units import *
 
 
 def rolling_avg(traj: np.ndarray, n: int = 50) -> np.ndarray:
@@ -65,53 +66,6 @@ def welford_var(
     return mean, M2, var
 
 
-def delta_pmf(
-    pmf: np.ndarray, T: float = 300.0, min_bin: int = 20, max_bin: int = -20
-) -> tuple:
-    """calculate free energy difference
-
-    args:
-        pmf: potential of mean force (free energy surface)
-        T: temperature
-        min_bin/max_bin: minimum/maximum bin for search of transition state
-
-    returns:
-        dA (float): free energy difference
-        dA_grid (np.ndarray): free energy difference on grid
-    """
-    R = 8.314 / 1000.0  # kJ / K mol
-    RT = (R * T) / 1000.0
-    pmf = pmf[~np.isnan(pmf)]
-
-    TS = np.where(pmf == np.amax(pmf[min_bin:max_bin]))[0][0]
-
-    P = np.exp(-pmf / RT)
-    P /= P.sum()
-
-    P_a = P[:TS].sum()
-    P_b = P[(TS + 1) :].sum()
-
-    dA = RT * np.log(P_a / P_b)
-    dA_grid = np.zeros(len(pmf))
-    dA_grid[TS] += dA / 2
-    dA_grid[(TS + 1) :] += dA
-
-    return dA, dA_grid
-
-
-def _error_vec(
-    n_frames: np.ndarray, beta_Ai: np.ndarray, exp_U: np.ndarray
-) -> np.ndarray:
-    """error vector for MBAR"""
-    denominator = np.multiply(n_frames * np.exp(beta_Ai), exp_U.T)
-    denominator = 1.0 / np.sum(denominator, axis=1)
-    # sum over all frames
-    error_v = n_frames - n_frames * np.exp(beta_Ai) * (
-        np.multiply(exp_U, denominator).sum(axis=1)
-    )
-    return error_v
-
-
 def _join_frames(traj_list: List[np.array]) -> Tuple[np.ndarray, float, np.ndarray]:
     """get one array with all frames from list of trajectories"""
     num_frames = 0
@@ -125,3 +79,128 @@ def _join_frames(traj_list: List[np.array]) -> Tuple[np.ndarray, float, np.ndarr
 
     frames_per_traj = np.array(frames_per_traj)
     return all_frames, num_frames, frames_per_traj
+
+
+def ensemble_average(obs: np.ndarray, weights: np.ndarray) -> tuple:
+    """ensemble average of observable
+
+    args:
+        obs (np.ndarray): trajectory of observables
+        weights (np.ndarray): weigths of data frames
+
+    returns:
+        avg (float): ensemble average
+        sem (float): standard error of the mean of avg
+    """
+    weights /= weights.sum()
+    avg = np.average(obs, weights=weights)  # ensemble average
+    std = np.sqrt(
+        np.average(np.power(obs - avg, 2), weights=weights)
+    )  # standard deviation
+    sem = std / np.sqrt(float(len(obs)))  # standard error of the mean
+    return avg, sem
+
+
+def conditional_average(
+    grid: np.ndarray, xi_traj: np.ndarray, obs: np.ndarray, weights: np.ndarray
+) -> tuple:
+    """bin wise conditional average and standard error of the mean
+
+    args:
+        grid (np.ndarray): bins along reaction coordinate
+        xi_traj (np.ndarray): trajectory of reaction coordinate
+        obs (np.ndarray): trajectory of observables
+        weights (np.ndarray): weigths of data frames
+
+    returns:
+        obs_xi (np.ndarray): conditional average along reaction coordinate
+        sem_xi (np.ndarray): standard error of the mean of obs_xi
+    """
+    weights /= weights.sum()
+    dx = grid[1] - grid[0]
+    dx2 = dx / 2
+
+    obs_xi = np.zeros(len(grid))
+    sem_xi = np.zeros(len(grid))
+
+    for i, x in enumerate(grid):
+        dat_i = np.where(np.logical_and(xi_traj >= x - dx2, xi_traj < x + dx2))
+        nsamples = len(obs[dat_i])
+        if nsamples > 0:
+            p_i = weights[dat_i] / weights[dat_i].sum()
+            obs_xi[i] = np.sum(p_i * obs[dat_i])
+            sem_xi[i] = np.sqrt(
+                np.sum(p_i * np.square(obs[dat_i] - obs_xi[i]))
+            ) / np.sqrt(nsamples)
+    return obs_xi, sem_xi
+
+def reaction_freeE(
+        pmf: np.ndarray, T: float = 300.0, min_bin: int = 20, max_bin: int = -20, TS: int = None
+) -> tuple:
+    """calculate free energy difference
+       see: Dietschreit et al., J. Chem. Phys. 156, 114105 (2022); https://doi.org/10.1063/5.0083423
+    args:
+        pmf: potential of mean force (free energy surface)
+        T: temperature
+        min_bin/max_bin: minimum/maximum bin for search of transition state
+        TS: alternatively, bin number of TS
+    returns:
+        dA (float): free energy difference
+        dA_grid (np.ndarray): free energy difference on grid
+    """
+    RT = (R_in_SI * T) / 1000.0
+    pmf = pmf[~np.isnan(pmf)]
+    
+    if TS == None:
+        TS = np.where(pmf == np.amax(pmf[min_bin:max_bin]))[0][0]
+
+    P = np.exp(-pmf / RT)
+    P /= P.sum()
+
+    P_a = P[:TS].sum()
+    P_b = P[(TS + 1) :].sum()
+
+    dA = RT * np.log(P_a / P_b)
+    dA_grid = np.zeros(len(pmf))
+    dA_grid[TS] += dA / 2
+    dA_grid[(TS + 1) :] += dA
+    dA_approx = pmf[TS:].min() - pmf[:TS].min()
+
+    return dA, dA_grid, dA_approx
+
+def activation_freeE(
+        pmf: np.ndarray, m_xi_inv: np.ndarray, T: float = 300.0, min_bin: int = 20, max_bin: int = -20, TS: int = None
+) -> tuple:
+    """calculate activation free energy
+       see: Dietschreit et al., J. Chem. Phys. XX, XXX (2022); https://doi.org/XXXX
+    args:
+        pmf: potential of mean force (free energy surface)
+        m_xi_inv: z-conditioned average of inverse mass associates with CV, expected units are xi^2/(au_mass * angstrom^2)
+        T: temperature
+        min_bin/max_bin: minimum/maximum bin for search of transition state
+        TS: alternatively, bin number of TS
+    returns:
+        dA (float): free energy difference
+        dA_grid (np.ndarray): free energy difference on grid
+    """
+    RT = (R_in_SI * T) / 1000.0  # kJ/mol
+    
+    pmf = fep[~np.isnan(fep)]
+    if TS == None:
+        TS = np.where(pmf == np.amax(pmf[min_bin:max_bin]))[0][0]
+    
+    rho = np.exp(-pmf/RT)
+    P = rho / rho.sum() # normalize so that P_a + P_b = 1.0
+    
+    lambda_xi = np.sqrt((h_in_SI * h_in_SI * m_xi_inv[TS]) / (2. * np.pi * atomic_to_kg  * kB_in_SI * T))
+    lambda_xi *= 1e10
+
+    P_a = P[:TS].sum()
+    P_b = P[(TS+1):].sum()
+
+    dA_a2b = - RT*np.log((rho[TS]*lambda_xi) / P_a)
+    dA_b2a = - RT*np.log((rho[TS]*lambda_xi) / P_b)
+    dA_a2b_approx = pmf[TS] - pmf[:TS].min()
+    dA_b2a_approx = pmf[TS] - pmf[TS:].min()
+    
+    return dA_a2b, dA_b2a, dA_a2b_approx, dA_b2a_approx

adaptive_sampling/sampling_tools/enhanced_sampling.py
@@ -238,8 +238,7 @@ class EnhancedSampling(ABC):
     def _kinetics(self, delta_xi):
         """accumulates data for kinetics"""
         forces = self.the_md.get_sampling_data().forces
-        m_xi = self._get_mass_of_cv(delta_xi)
-        self.mass_traj.append(m_xi)
+        self.mass_traj.append(self._get_mass_of_cv(delta_xi))
         self.abs_forces.append(np.linalg.norm(forces))
         self.CV_crit_traj.append(np.dot(delta_xi[0], forces))
         self.abs_grad_xi.append(np.linalg.norm(delta_xi))
@@ -252,10 +251,10 @@ class EnhancedSampling(ABC):
             delta_xi: gradients of cv's
 
         Returns:
-            m_xi: coordinate dependent mass of collective variabl
+            m_xi_inv: coordinate dependent mass of collective variabl
         """
         if self.ncoords == 1:
-            return np.dot(delta_xi[0], 1.0 / self.the_md.mass * delta_xi[0])
+            return np.dot(delta_xi[0], (1.0 / self.the_md.mass) * delta_xi[0])
         else:
             return 0.0
 
@@ -321,7 +320,7 @@ class EnhancedSampling(ABC):
             for kw in data.keys():
                 traj_out.write("%14s\t" % kw)
             if self.kinetics:
-                traj_out.write("%14s\t" % "m_xi [a.u.]")
+                traj_out.write("%14s\t" % "m_xi_inv [a.u.]")
                 traj_out.write("%14s\t" % "|dU| [a.u.]")
                 traj_out.write("%14s\t" % "|dxi| [a.u.]")
                 traj_out.write("%14s\t" % "dU*dxi [a.u.]")
@@ -333,7 +332,8 @@ class EnhancedSampling(ABC):
             for n in range(self.out_freq):
                 traj_out.write(
                     "\n%14.6f\t"
-                    % ((step - self.out_freq + n) * self.the_md.dt * 1.0327503e0)
+                    % ((step - self.out_freq + n) * self.the_md.dt * atomic_to_fs
+                    )
                 )  # time in fs
                 for i in range(len(self.traj[0])):
                     traj_out.write("%14.6f\t" % (self.traj[-self.out_freq + n][i]))

adaptive_sampling/units.py
@@ -7,9 +7,10 @@ atomic_to_K        = 315775.04e0
 BOHR_to_ANGSTROM   =  0.52917721092e0
 DEGREES_per_RADIAN = 180.0 / np.pi
 atomic_to_kg       = 1.66054e-27
-h_in_SI            = 6.62607004e-34 
+atomic_to_fs       = 1.0327503e0
 
 # constants
 kB_in_SI     = 1.380648e-23 
 R_in_SI      = 8.314
 kB_in_atomic = kB_in_SI * J_to_atomic
+h_in_SI            = 6.62607004e-34 