e6977fc4ec6e6c7c760ac111deb990e04366e1d4
ahulm <andreas-hulm@posteo.de>
2022-12-01 09:11:25 +0100

    gawtmeabf tests

--- adaptive_sampling/sampling_tools/gawtmeabf.py
+++ adaptive_sampling/sampling_tools/gawtmeabf.py
@@ -52,7 +52,14 @@ class GaWTMeABF(WTMeABF, GaMD, EnhancedSampling):
         self.do_wtm = do_wtm
         self.qm_boost = qm_boost
 
-    def step_bias(self, write_output: bool = True, write_traj: bool = True, **kwargs):
+    def step_bias(
+        self, 
+        write_output: bool = True, 
+        write_traj: bool = True, 
+        output_file: str = 'gawtmeabf.out',
+        traj_file: str = 'CV_traj.dat', 
+        restart_file: str = 'restart_gawtmeabf',
+        **kwargs):
 
         md_state = self.the_md.get_sampling_data()
         epot = md_state.epot
@@ -159,7 +166,7 @@ class GaWTMeABF(WTMeABF, GaMD, EnhancedSampling):
         if md_state.step % self.out_freq == 0:
 
             if write_traj:
-                self.write_traj()
+                self.write_traj(filename=traj_file)
 
             if write_output:
                 self.get_pmf()
@@ -170,8 +177,8 @@ class GaWTMeABF(WTMeABF, GaMD, EnhancedSampling):
                 output[f"metapot"] = self.metapot
                 output[f"GaMD corr"] = self.gamd_corr
 
-                self.write_output(output, filename="gaeabf.out")
-                self.write_restart()
+                self.write_output(output, filename=output_file)
+                self.write_restart(filename=restart_file)
 
         return bias_force
 
@@ -285,7 +292,7 @@ class GaWTMeABF(WTMeABF, GaMD, EnhancedSampling):
         if self.verbose:
             print(f" >>> Info: Adaptive sampling restartet from {filename}!")
 
-    def write_traj(self):
+    def write_traj(self, filename: str = 'CV_traj.dat'):
         """save trajectory for post-processing"""
 
         data = self._write_ext_traj()
@@ -293,7 +300,7 @@ class GaWTMeABF(WTMeABF, GaMD, EnhancedSampling):
         data["Epot [H]"] = self.epot
         data["T [K]"] = self.temp
 
-        self._write_traj(data)
+        self._write_traj(data, filename=filename)
 
         # reset trajectories to save memory
         self.traj = np.array([self.traj[-1]])
