a6503f4ab5abf5a67f87aead9625c4020daa24fc
ahulm <andreas-hulm@posteo.de>
2023-12-22 12:16:55 +0100

    FENEB done

--- adaptive_sampling/sampling_tools/feneb.py
+++ adaptive_sampling/sampling_tools/feneb.py
@@ -15,13 +15,29 @@ class FENEB:
     """Free Energy NEB method for path optimisation in cartesian coordinates
 
     Based on: Semelak, Jonathan A., et al. JCTC (2023): <https://pubs.acs.org/doi/full/10.1021/acs.jctc.3c00366>
+
+    Args:
+        fep_k: force constant for umbrella integration, has to match force constant of confinement in MD simulations
+        neb_k: force constant of spring force
+        extremes: List of filenames of xyz file for reactand and product structure
+        path: `.npy` file with NEB nodes, if None, a new path will be created by interpolation between extremes
+        nimages: number of NEB images without extremes
+        conf_spring: convergence criterion for uncouled band optimization
+        maxiter_spring: maximum iterations for uncoupled band optimization
+        active: Indices of active atoms that build the cartesian reaction coordinate space (RCS)
+        idpp: if True, use improved path interpolation with IDPP method
+        load_from_dcd: if True, load MD data form DCD file, otherwise a simple `.dat` with RCS data in columns is assumed
+        top: Topology filename (e.g. .pdb, .prmtop, .parm7), required to load DCD
+        write_xyz: write path nodes to xyz files
+        convert_units: convert Angstrom to Bohr for all corresponding inputs
+        verbose: print verbose information
     """
     def __init__(
         self,
         fep_k: float,
         neb_k: float,
-        endpoints: List[str],
-        old_path: np.ndarray=None,
+        extremes: List[str],
+        path: str=None,
         nimages: int=10,
         conf_spring: float=0.1,
         maxiter_spring: int=1000,
@@ -30,10 +46,17 @@ class FENEB:
         load_from_dcd: bool=True,
         top: str=None,
         write_xyz: bool=False,
+        convert_units: bool=True,
         verbose: bool=False,
     ):
-        self.neb_k = neb_k
-        self.fep_k = fep_k 
+        self.convert_units = convert_units
+        if convert_units:
+            self.neb_k = neb_k * BOHR_to_ANGSTROM * BOHR_to_ANGSTROM  
+            self.fep_k = fep_k * BOHR_to_ANGSTROM * BOHR_to_ANGSTROM 
+        else:
+            self.neb_k = neb_k
+            self.fep_k = fep_k
+
         self.verbose = verbose
         self.active = active
         self.conf_spring = conf_spring
@@ -49,22 +72,24 @@ class FENEB:
             
             self.top = top
             if self.top == None:
-                print('WARNING: you need to give a topology (e.g. pdb or prmtop) for dcd import!')
+                print(' >>> FENEB: you need to give a topology (e.g. pdb or prmtop) for dcd import!')
             if self.verbose:
-                print(f'INFO: Topology for dcd import: {self.top}')
+                print(f' >>> FENEB: Topology for dcd import: {self.top}')
         
         self.optstep = 0
 
-        self.initial = io.read(endpoints[0]) # always get on ase.Atoms object for io operations
+        self.initial = io.read(extremes[0]) # always store one ase.Atoms object for io operations
         if self.active == None:
             self.active = [i for i in range(len(self.initial.get_atomic_numbers()))]
         self.natoms = len(self.initial.get_atomic_numbers())
 
-        if old_path == None:
-            self.final   = io.read(endpoints[1])
-            self.path = self.make_path(idpp, write_xyz=write_xyz)
+        if path == None:
+            self.final   = io.read(extremes[1])
+            self.path = self.make_path(idpp)
+            if write_xyz:
+                self.write_nodes_to_xyz()
         else:
-            self.path = np.load(old_path)
+            self.path = np.load(path)
             self.path = [node for node in self.path]
 
     def step(self, feg: np.ndarray=None, uncoupled_band_opt: bool=True) -> dict:
@@ -96,7 +121,7 @@ class FENEB:
                 
             force = force.reshape((self.natoms,3))
 
-            force_norm_max, scaling_factor = self._get_scaling_factor(force)
+            force_norm_max, scaling_factor = self._get_scaling_factor(force, convert_units=self.convert_units)
             if force_norm_max > 0.0:
                 self.path[i+1][self.active] -= scaling_factor * force[self.active] / force_norm_max 
             else:
@@ -114,11 +139,11 @@ class FENEB:
         if self.verbose:
             if uncoupled_band_opt:
                 print(
-                    f'Iter {self.optstep:3d}:\tMax(Forces) = {np.max(confs_fes):3.6f} (idx: {np.argmax(confs_fes)+1:3d}),\tMax(F_spring) = {np.max(conf_spring):3.6f} (Iters: {iter_spring:3d})'
+                    f' >>> FENEB: Iter {self.optstep:3d}:\tMax(Forces) = {np.max(confs_fes):3.6f} (idx: {np.argmax(confs_fes)+1:3d}),\tMax(F_spring) = {np.max(conf_spring):3.6f} (Iters: {iter_spring:3d})'
                 )
             else:
                 print(
-                    f'Iter {self.optstep:3d}:\tMax(Forces) = {np.max(confs_fes):3.6f} (idx: {np.argmax(confs_fes)+1:3d})'
+                    f' >>> FENEB: Iter {self.optstep:3d}:\tMax(Forces) = {np.max(confs_fes):3.6f} (idx: {np.argmax(confs_fes)+1:3d})'
                 )
 
         results = {
@@ -129,11 +154,14 @@ class FENEB:
         }
         return results
 
-    def opt_node_spacing(self, verbose: bool=False):
+    def opt_node_spacing(self, outfreq: int=100):
         """ full optimization of spring force """
 
         conf_spring = 9999
-        iter_spring = 0
+        iter_spring = -1
+
+        if self.verbose:
+            print(" >>> FENEB: Starting full optimization of spring force")
 
         while True:
             
@@ -152,7 +180,7 @@ class FENEB:
                     (self.path[i+1][self.active].flatten()-self.path[i+0][self.active].flatten()), tau) * tau
                 fspring = fspring.reshape((self.natoms,3))
 
-                conf, spring_scaling = self._get_scaling_factor(fspring)
+                conf, spring_scaling = self._get_scaling_factor(fspring, convert_units=self.convert_units)
                 confs_spring.append(conf)
 
                 if conf > 0.0:
@@ -165,14 +193,18 @@ class FENEB:
             conf_spring = np.max(confs_spring)
             self.path = tmp_path.copy()
 
+            if iter_spring%outfreq == 0:
+                if self.verbose:
+                    print(f" >>> Iter: {iter_spring:5d}, Max(F_spring): {conf_spring:14.6f} (node index: {np.argmax(confs_spring)})")
+            
             if conf_spring < self.conf_spring:
-                if verbose:
-                    print(f" >>> INFO: Spring optimization converged after {iter_spring} iterations.")
+                if self.verbose:
+                    print(f" >>> FENEB: Spring optimization converged after {iter_spring} iterations.")
                 break
             
             if self.maxiter_spring <= iter_spring:
-                if verbose:
-                    print(f" >>> INFO: Final spring force after {iter_spring} steps: {conf_spring}.")
+                if self.verbose:
+                    print(f" >>> FENEB: Final spring force after {iter_spring} steps: {conf_spring}.")
                 break
 
         return conf_spring, iter_spring
@@ -191,17 +223,26 @@ class FENEB:
         return tau.flatten()       
 
     @staticmethod
-    def _get_scaling_factor(forces):
+    def _get_scaling_factor(forces, convert_units: bool=True):
+        """ Get SD scaling based on heuristic criterion
+        """
         norm_max = np.max(np.linalg.norm(forces.reshape((int(len(forces.flatten())/3),1,3)), axis=2))
+
+        if convert_units:
+            conversion = BOHR_to_ANGSTROM
+        else: 
+            conversion = 1.0                            
+
         if norm_max > 5.0:
-            scaling_factor = 0.005 / BOHR_to_ANGSTROM
+            scaling_factor = 0.005 / conversion
         elif norm_max > 2.5:
-            scaling_factor = 0.002 / BOHR_to_ANGSTROM
+            scaling_factor = 0.002 / conversion
         else:
-            scaling_factor = 0.0001 / BOHR_to_ANGSTROM
+            scaling_factor = 0.0001 / conversion
+
         return norm_max, scaling_factor
 
-    def make_path(self, idpp: bool, write_xyz: bool=True) -> List[np.ndarray]:
+    def make_path(self, idpp: bool) -> List[np.ndarray]:
         """Build initial path by linear interpolation between endpoints
 
         Args:
@@ -213,13 +254,16 @@ class FENEB:
         images += [self.final]
 
         if len(self.active) != len(images[0].get_positions()):
+            
             if self.verbose:
                 print(" >>> FENEB: Fixing atom positions that are not `active` in path interpolation")
-            from ase.constraints import FixAtoms
+            
             apply_constraint = True
+            from ase.constraints import FixAtoms
             c = FixAtoms(indices=np.delete(np.arange(len(images[0].get_positions())), self.active))
             for img in images:
                 img.set_constraint(c)
+                
         else:
             apply_constraint = False
 
@@ -228,11 +272,11 @@ class FENEB:
             neb.interpolate('idpp', apply_constraint=apply_constraint)
         else:
             neb.interpolate(apply_constraint=apply_constraint)
-        
-        if write_xyz:
-            self.write_nodes_to_xyz()
 
-        return [image.get_positions() / BOHR_to_ANGSTROM for image in images]
+        if self.convert_units:
+            return [image.get_positions() / BOHR_to_ANGSTROM for image in images]
+        else:
+            return [image.get_positions() for image in images]
 
     def calc_feg(self, startframe: int=0, move_nodes_to_mean: bool=True, dim: int=3) -> np.ndarray:
         """Get free energy gradient for Umbrella Integration
@@ -253,12 +297,13 @@ class FENEB:
             if self.verbose:
                 print(' >>> FENEB: recentering path nodes on trajectory averages')
             tmp = self.path.copy()
-            self.path = []
-            for i, node in enumerate(mean_coords):
+            self.path = [tmp[0]]
+            for i, node in enumerate(mean_coords[1:-1]):
                 if node.sum() != 0.0:
                     self.path.append(node)
                 else:
-                    self.path.append(tmp[i])
+                    self.path.append(tmp[i+1])
+            self.path.append(tmp[-1])
         elif self.verbose:
             print(' >>> FENEB: path nodes not recentered')
 
@@ -288,14 +333,15 @@ class FENEB:
             path = os.path.join(datapath, f"{i}/{traj_filename}")
             if os.path.isfile(path):
                 if self.load_from_dcd:
-                    self.data.append([i, md.load(path, top=self.top).xyz])
+                    # mdtraj loads data in nm!
+                    self.data.append([i, md.load(path, top=self.top).xyz * 10.0 / BOHR_to_ANGSTROM])
                 else:
                     self.data.append([i, np.loadtxt(path)])
                 if self.verbose:
-                    print(f" >>> FENEB: Loaded data from {os.path.join(path, traj_filename)}.")
+                    print(f" >>> FENEB: Loaded data from {path}")
             else: 
                 if self.verbose:
-                    print(f" >>> FENEB: No data in {path}.")
+                    print(f" >>> FENEB: No data in {path}")
     
         # Sort the simulations based on their image ID
         self.data.sort(key=lambda x: x[0])
@@ -306,7 +352,10 @@ class FENEB:
         """ Saves one `.xyz` file for each path node
         """
         for i, coords in enumerate(self.path):     
-            self.initial.set_positions(coords * BOHR_to_ANGSTROM)
+            if self.convert_units:
+                self.initial.set_positions(coords * BOHR_to_ANGSTROM)
+            else:
+                self.initial.set_positions(coords)
             io.write(f"{i}.xyz", self.initial)
 
 
